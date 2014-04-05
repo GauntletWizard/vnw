e0d189160223ced568ec2bce4c5d6509e9fe53e3
Thomas Hahn <Thahn@tcbtech.com>
2014-04-05 03:57:49 +0000

    UI bits

--- ui/ui.go
+++ ui/ui.go
@@ -1,9 +1,35 @@
 package ui
 
 import (
-	_ "net/http"
+	"net/http"
+  "flag"
+  "vnw/config"
+  "html/template"
 )
 
-func Start() {
-  for {}
+var httplistener = flag.String("port", ":80", "Listen Address for webserver")
+
+func init() {
+  flag.Parse()
 }
+
+func Start() {
+  http.HandleFunc("/cards", listCards)
+  http.HandleFunc("/status", statuss)
+  http.ListenAndServe(*httplistener, nil)
+}
+
+func listCards(w http.ResponseWriter, r *http.Request) {
+  for i, j := range *config.Cards {
+    w.Write(i, j)
+  }
+}
+
+func statuss(w http.ResponseWriter, r *http.Request) {
+  t, err := template.New("statuszs").Parse("<!DOCTYPE HTML><html><head><title>Status Page for Lock Mechanism")
+  if err != nil {
+    log.Error("Your template is bad, and you should feel bad!", err)
+  }
+}
+
+
