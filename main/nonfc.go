6b30742e7f94906798de7ca8edd0ed084bb3f389
Thomas Hahn <Thahn@tcbtech.com>
2014-04-06 03:25:36 +0000

    More flag cleanup bits.

---
+++ main/nonfc.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"vnw/config"
+  "vnw/gpio"
+	//	"vnw/nfc"
+	"vnw/ui"
+  "fmt"
+  "flag"
+)
+func init() {
+  flag.StringVar(&ui.Httplistener, "port", ":80", "Listen Address for webserver")
+  flag.StringVar(&gpio.Gpiodir, "gpiodir", "/sys/class/gpio", "Directory that holds GPIO pins. Exported for testing.")
+  fmt.Println("Log message")
+}
+
+func main() {
+  flag.Parse()
+  gpio.Setup()
+  fmt.Println("Starting config")
+	config.Start()
+	//	nfc.Poll()
+  fmt.Println("Starting UI Server")
+	ui.Start()
+  fmt.Println("Shouldn't reach this")
+}
