992947bcae3b8218dda0874192721d4e131d948d
Thomas Hahn <Thahn@tcbtech.com>
2014-04-07 18:49:23 -0700

    Updated lock and unlock programs with new flag style

--- main/unlock.go
+++ main/unlock.go
@@ -1,7 +1,11 @@
 package main
 
 import "vnw/gpio"
+import "flag"
 
 func main() {
+	flag.StringVar(&gpio.Gpiodir, "gpiodir", "/sys/class/gpio", "Directory that holds GPIO pins. Exported for testing.")
+	flag.IntVar(&gpio.Pin, "gpiopin", 60, "GPIO Pin to use")
+	flag.Parse()
 	gpio.High()
 }
