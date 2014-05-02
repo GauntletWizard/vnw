098db01e299f44df0b1d574d6c9bb144da2b3a65
Thomas Hahn <Thahn@tcbtech.com>
2014-05-02 14:35:57 -0700

    Merged

--- scripts/qemu.sh
+++ scripts/qemu.sh
@@ -52,9 +52,11 @@ install $VNW/scripts/firstrun $PIMNT/etc/init.d/
 ln -s /etc/init.d/firstrun $PIMNT/etc/rc2.d/S90firstrun
 install -d $PIMNT/service/
 install -o ted $VNW/scripts/run $PIMNT/service/main/run
-#piumount
+# Clear state.
+rm $VNW/var/opt/vnw-run
+piumount
 # This is close, but not necessarially right. cigar; hrmm.
 # truncate -s 4008706048
 }
 
-vnw
+pimount
